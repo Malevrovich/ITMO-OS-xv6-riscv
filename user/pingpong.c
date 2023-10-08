06a9544b72d1a63a7fe7a19209113ca8a6904ff6
malevrovich <dddccc20000@mail.ru>
2023-10-09 01:03:25 +0300

    Pingpong review fixes

--- user/pingpong.c
+++ user/pingpong.c
@@ -2,24 +2,69 @@
 #include "user/user.h"
 
 int
-main(void) 
+main(void)
 {
-    int p[2];
-    char buf[5];
+  int p[2][2];
+  char to_child_msg[] = "ping";
+  char to_parent_msg[] = "pong";
 
-    pipe(p);
+  if(pipe(p[0]) >= 0){
+    if(pipe(p[1]) >= 0){
+      int pid = fork();
 
-    if (fork() == 0) {
-        read(p[0], buf, sizeof(buf));
-        printf("%d: got %s\n", getpid(), buf);
+      if(pid == 0){
+        close(p[0][1]);
+        close(p[1][0]);
 
-        write(p[1], "pong", sizeof(buf));
+        for(uint64 i = 0; i < sizeof(to_child_msg); i++){
+          to_child_msg[i] -= 1;
+        }
+        if(read(p[0][0], to_child_msg, sizeof(to_child_msg)) < sizeof(to_child_msg)){
+          printf("Not enough bytes were read in %d\n", getpid());
+          exit(-1);
+        }
+
+        close(p[0][0]);
+        printf("%d: got %s\n", getpid(), to_child_msg);
+
+        if(write(p[1][1], to_parent_msg, sizeof(to_parent_msg)) < sizeof(to_parent_msg)){
+          printf("Not enough bytes were written in %d\n", getpid());
+          exit(-1);
+        }
+        close(p[1][1]);
+      } else if(pid > 0){
+        close(p[0][0]);
+        close(p[1][1]);
+
+        if(write(p[0][1], to_child_msg, sizeof(to_child_msg)) < sizeof(to_child_msg)){
+          printf("Not enough bytes were written in %d\n", getpid());
+          exit(-1);
+        }
+        close(p[0][1]);
+
+        for(uint64 i = 0; i < sizeof(to_parent_msg); i++){
+          to_parent_msg[i] -= 1;
+        }
+        if(read(p[1][0], to_parent_msg, sizeof(to_parent_msg)) < sizeof(to_parent_msg)){
+          exit(-1);
+        }
+        close(p[1][0]);
+
+        printf("%d: got %s\n", getpid(), to_parent_msg);
+      } else {
+        printf("Fork failed\n");
+        exit(-1);
+      }
+
+      exit(0);
     } else {
-        write(p[1], "ping", 5);
-
-        read(p[0], buf, sizeof(buf));
-        printf("%d: got %s\n", getpid(), buf);
+      printf("Second pipe failed\n");
+      close(p[0][1]);
+      close(p[0][0]);
+      exit(-1);
     }
-
-    exit(0);
+  }
+  printf("First pipe failed\n");
+  exit(-1);
+  return -1;
 }
